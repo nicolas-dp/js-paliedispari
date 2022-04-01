36d7e69f83dd96dd606fe4a3b5fcb672df6789e5
Nicolas <wellcomenicolas@gmail.com>
2022-04-01 17:42:37 +0200

    Final Project

--- main.js
+++ main.js
@@ -3,31 +3,34 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire 
 se la parola inserita è palindroma */
 
-/* const parolaPalindroma = prompt("Inserire una parola")
- */
+const parolaPalindroma = prompt("Inserire una parola");
+ 
 
 /**
  * 
- * @param {string} -   
+ * @param {string} -  Passiamo il paramentro indicato dall'utente 
  * @returns {string} - Fornisce l'estio della stringa nel caso sia o no palindroma
  */
-/* function palindroma(parolaPalindroma) {
-
+function palindroma(parolaPalindroma) {
+    //Prendiamo la lunghezza della parola passata dall'utente
     let lunghezza = parolaPalindroma.length;
+    //Andiamo a dividere la lunghezza a metà e la mettiamo in una variabile middle
     let middle = Math.floor(lunghezza/2);
 
+    //Tramite ciclo for, cicliamo per verificare l'uguaglianza dei caratteri
     for ( let i = 0; i < middle; i++ ) {
+        //Se le prime due lettere sono diverse dalle ultime due lettere
         if (parolaPalindroma[i] !== parolaPalindroma[lunghezza - 1 - i]) {
             return `La parola ${parolaPalindroma.toUpperCase()} non è palindroma`;
-        }
+        }        
     }
-    
+   //Se le prime due lettere sono ugali alle ultime due lettere 
    return `La parola ${parolaPalindroma.toUpperCase()} è palindroma`;
     
 } 
 
-
-console.log(palindroma(parolaPalindroma));*/
+//Richiamo della funzione palindroma
+console.log(palindroma(parolaPalindroma));
 
 
 
@@ -43,14 +46,58 @@ Dichiariamo chi ha vinto. */
 
 let pari_dispari = prompt("Scegli pari o dispari!!")
 let numero_utente = parseInt(prompt("Inserisci un numero da 1 a 5"))
-const min_number = 1;
-const max_number = 5;
+let somma = 0;
 
-function generaNumeroRandom(min, max) {
+//console.log(`L'utente ha scelto:  ${pari_dispari}`);
+
+if (pari_dispari > 5 || pari_dispari < 1) {
+    alert("Devi inserire un numero compreso tra 1 e 5")
+}
+
+/**
+ * 
+ * @param {number} - min Il numero minimo 1  
+ * @param {number} - max Il numero massimo 5
+ * @returns {number} 
+ */
+ function numeroCasuale(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
 
-console.log(generaNumeroRandom(min_number, max_number));
-let somma = generaNumeroRandom(min_number, max_number) + numero_utente;
+//Richiamo funzione per genereare il numero casuale
+let random_number = numeroCasuale(1, 5)
+
+//Andiamo a sommare il numero inserito dall'utente e il numero random generato dalla funzione
+somma += random_number + numero_utente;
+
+/* INIZIO FUNZIONE PER VERIFICARE SE IL TOTALE DELLA SOMMA è PARI O DISPARI */
+/**
+ * 
+ * @param {number} - sum La somma tra il numero generato random e quello indicato dall'utente 
+ * @returns - true("è PARI") or false ("è DISPARI")
+ */
+function sePariDispari(sum) {
+    
+    if (sum % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+
+console.log(`Il numero dell'utente è: ${numero_utente}`);
+console.log(`Il numero scelto randomicamente è: ${random_number}`)
+console.log(`La somma dei due numeri è: ${sePariDispari(somma)}`);
+
+if (sePariDispari(somma) == true && pari_dispari == "pari") {
+    console.log("HAI VINTO");
+} else  {
+    console.log("HAI PERSO");
+}
+
+
+
 
 
